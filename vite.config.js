f7f3597a907fbf3c9f9371027515a72dbc410817
Artur Sardà Díez <asardadi19@alumnes.ub.edu>
2026-05-22 20:02:11 +0200

    Update base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,14 @@ import vue from "@vitejs/plugin-vue";
 import { fileURLToPath, URL } from "node:url";
 
 export default defineConfig({
-	plugins: [vue()],
-	resolve: {
-		alias: {
-			"@": fileURLToPath(new URL("./src", import.meta.url)),
-		},
-	},
-	server: {
-		open: true, // This will open the browser automatically
-	},
+  base: "/NutriMove/",
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    open: true,
+  },
 });
